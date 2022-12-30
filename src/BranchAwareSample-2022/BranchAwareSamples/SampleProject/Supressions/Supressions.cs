e66c2d495a25f9680bd9402153947baaa3d29403
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-12-30 13:49:33 +0100

    orphan build

---
+++ src/BranchAwareSample-2022/BranchAwareSamples/SampleProject/Supressions/Supressions.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SampleProject.Supressions
+{
+    internal class Supressions
+    {
+        //TODO: Suppressed in Main
+
+        //TODO: Supressed in ChildBranch
+
+        //TODO: Not Supressed in any Branch
+    }
+}
