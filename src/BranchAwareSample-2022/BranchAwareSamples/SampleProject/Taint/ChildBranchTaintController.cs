3d9eed96f46a2aa645d55caeaa9bce27ec426d88
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-12-30 15:30:50 +0100

    Taint for child Branch

---
+++ src/BranchAwareSample-2022/BranchAwareSamples/SampleProject/Taint/ChildBranchTaintController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SampleProject.Taint
+{
+    public class ChildBranchTaintController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Simple taint issue - just involves this method
+        public ActionResult DeleteFile(string fileName)
+        {
+            System.IO.File.Delete(fileName); // Noncompliant
+
+            return Content("File " + fileName + " deleted");
+        }
+    }
+}
